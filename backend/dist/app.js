d5806a3f5fe6adc6d6448aeb66686a9cc06cb145
arkajyotiadhikary <arkajyotiadhikary15@gmail.com>
2024-08-28 00:45:50 +0530

    Private Routes

--- backend/dist/app.js
+++ backend/dist/app.js
@@ -1,6 +1,10 @@
 import express from 'express';
+import cors from 'cors';
 // Routes
 import userRouter from "./routes/user.route.js";
 const app = express();
+// Middlewares
+app.use(cors());
+app.use(express.json());
 app.use('/', userRouter);
 export default app;
--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,13 @@
 import express from 'express';
-
+import cors from 'cors';
 // Routes
 import userRouter from "./routes/user.route.js";
 
 const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
 
 app.use('/', userRouter);
 
